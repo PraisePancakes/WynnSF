2bf3f6f5b1dc0df56c7474fc081d42701d07ee48
PraisePancakes <100659653+PraisePancakes@users.noreply.github.com>
2024-06-19 22:39:34 -0400

    implemented some basic questbook ui for the quests and their statuses

--- WynnSF/core/utils/utils.hpp
+++ WynnSF/core/utils/utils.hpp
@@ -1,14 +1,17 @@
 #pragma once
 #include <utility>
-#include <ranges>
+
 
 namespace Core {
 	namespace Utils {
 		float fisqrt(float num);
-
+		/*
 		constexpr inline auto enum_range = [](auto front, auto back) {
 			return std::views::iota(std::to_underlying(front), std::to_underlying(back) + 1)
 				| std::views::transform([](auto e) { return decltype(front)(e); });
 			};
+		
+		*/
+		
 	}
 }
--- WynnSF/src/QuestBook.hpp
+++ WynnSF/src/QuestBook.hpp
@@ -2,7 +2,9 @@
 #include "Quest.hpp"
 #include "../core/Manager/EntityManager.hpp"
 
-constexpr float SPRITE_BOOK_PADDING = 10;
+
+static constexpr float SPRITE_BOOK_PADDING = 10;
+static const std::string font_path = "src/Assets/Fonts/PixelFont.ttf";
 
 class QuestBook {
 	QuestDB* db{ nullptr };
@@ -10,18 +12,110 @@ class QuestBook {
 	sf::RenderWindow* ctx{ nullptr };
 
 
-public:
+	void _updateQuestBookSpritePos() {
+		sf::View view = ctx->getView();
+		std::shared_ptr<Entity> book = EntityManager::GetInstance()->GetEntities("QuestBook")[0];
 
-	QuestBook(QuestDB* db, sf::RenderWindow* ctx) {
-		this->db = db;
-		this->ctx = ctx;
+		auto sc = book->GetComponent<CSprite>();
+
+		const sf::Vector2f center = view.getCenter();
+		const sf::Vector2f topLeft((center.x - (view.getSize().x / 2)) + SPRITE_BOOK_PADDING, (center.y - (view.getSize().y / 2)) + SPRITE_BOOK_PADDING);
+		const sf::Vector2f bookSpriteOffset(32, 32);
+
+		sc->sprite.setPosition(topLeft.x + bookSpriteOffset.x, topLeft.y + bookSpriteOffset.y);
+	}
+
+	void _updateReturnButtonPos() {
+		sf::View view = ctx->getView();
+		std::shared_ptr<Entity> close = EntityManager::GetInstance()->GetEntities("QuestBook-Close")[0];
+
+		auto sc = close->GetComponent<CSprite>();
+
+		const sf::Vector2f center = view.getCenter();
+		const sf::Vector2f topLeft((center.x - (view.getSize().x / 2)) + SPRITE_BOOK_PADDING, (center.y - (view.getSize().y / 2)) + SPRITE_BOOK_PADDING);
+		const sf::Vector2f bookSpriteOffset(32, 32);
+
+		sc->sprite.setPosition(topLeft.x + bookSpriteOffset.x, topLeft.y + bookSpriteOffset.y);
+	}
+
+	
+
+	void _updateQuestTitlesPos() {
+		auto entityVec = EntityManager::GetInstance()->GetEntities("Quests");
+
+		sf::View view = ctx->getView();
+
+		static int padY = 0;
+		for (auto& e : entityVec) {
+			auto txt = e->GetComponent<CText>();
+			txt->text.setPosition(view.getCenter().x - (view.getSize().x / 2) + 100, view.getCenter().y - (view.getSize().y / 2) + padY);
+			padY += 25;
+		}
+
+		if (padY == entityVec.size() * padY) {
+			padY = 0;
+		}
+	}
+
+	void _updateQuestTitleStatus() {
+		auto entityVec = EntityManager::GetInstance()->GetEntities("Quests");
+		
+
+		size_t questIdx = 0;
+
+		for (auto& q : this->db->GetTable()){
+			if (q.second->state.isCompleted()) {
+			
+				auto txt = entityVec[questIdx]->GetComponent<CText>();
+				txt->text.setFillColor(sf::Color::Green);
+				
+			}
+
+			questIdx++;
+		}
+
+		
+	}
+
+	void _updateQuestBookOpened() {
+		_updateReturnButtonPos();
+		_updateQuestTitlesPos();
+		_updateQuestTitleStatus();
+		//check if quest is completed, make text green, check if text is in progress, make text yellow, and if locked, make red and display the min lvl required next to it
+
+		
+	}
+
+
+	void _initSprites() {
 		std::shared_ptr<Entity> book = EntityManager::GetInstance()->AddEntity("QuestBook");
 		book->AddComponent<CSprite>("src/Assets/UI/UI_QuestBook.png", sf::IntRect(0, 0, 128, 117), 64, 64);
 		book->AddComponent<CButton>(sf::RectangleShape(sf::Vector2f(64, 64)), sf::Vector2f(SPRITE_BOOK_PADDING, SPRITE_BOOK_PADDING), sf::Color::Transparent, sf::Color::Transparent);
 		std::shared_ptr<Entity> close = EntityManager::GetInstance()->AddEntity("QuestBook-Close");
 		close->AddComponent<CSprite>("src/Assets/UI/UI_Back.png", sf::IntRect(0, 0, 100, 100), 64, 64);
 		close->AddComponent<CButton>(sf::RectangleShape(sf::Vector2f(64, 64)), sf::Vector2f(SPRITE_BOOK_PADDING, SPRITE_BOOK_PADDING), sf::Color::Transparent, sf::Color::Transparent);
+	}
+
+	void _initUIQuests() {
+		std::map<int, Quest*> table = db->GetTable();
+		
+		for (auto& quest : table) {
+			auto questE = EntityManager::GetInstance()->AddEntity("Quests");
+			auto text = questE->AddComponent<CText>(quest.second->GetTitle(), font_path, 24, 0, 0, false);
+		}
+	}
+
+
+public:
+
+	QuestBook(QuestDB* db, sf::RenderWindow* ctx) {
+		this->db = db;
+		this->ctx = ctx;
+		_initSprites();
+		_initUIQuests();
 	};
+
+
 
 	bool IsOpen() const {
 		return _opened;
@@ -45,42 +139,10 @@ public:
 				this->_opened = false;
 				});
 		}
-		
 
-		
-		
 
 	}
 
-	void _updateQuestBookClosed() {
-		sf::View view = ctx->getView();
-		std::shared_ptr<Entity> book = EntityManager::GetInstance()->GetEntities("QuestBook")[0];
-
-		auto sc = book->GetComponent<CSprite>();
-
-		const sf::Vector2f center = view.getCenter();
-		const sf::Vector2f topLeft((center.x - (view.getSize().x / 2)) + SPRITE_BOOK_PADDING, (center.y - (view.getSize().y / 2)) + SPRITE_BOOK_PADDING);
-		const sf::Vector2f bookSpriteOffset(32, 32);
-
-		sc->sprite.setPosition(topLeft.x + bookSpriteOffset.x, topLeft.y + bookSpriteOffset.y);
-	}
-
-	void _updateQuestBookBack() {
-		sf::View view = ctx->getView();
-		std::shared_ptr<Entity> close = EntityManager::GetInstance()->GetEntities("QuestBook-Close")[0];
-
-		auto sc = close->GetComponent<CSprite>();
-
-		const sf::Vector2f center = view.getCenter();
-		const sf::Vector2f topLeft((center.x - (view.getSize().x / 2)) + SPRITE_BOOK_PADDING, (center.y - (view.getSize().y / 2)) + SPRITE_BOOK_PADDING);
-		const sf::Vector2f bookSpriteOffset(32, 32);
-
-		sc->sprite.setPosition(topLeft.x + bookSpriteOffset.x, topLeft.y + bookSpriteOffset.y);
-	}
-
-	void _updateQuestBookOpened() {
-		_updateQuestBookBack();
-	}
 
 	void Update() {
 		
@@ -88,7 +150,7 @@ public:
 			_updateQuestBookOpened();
 		}
 		else {
-			_updateQuestBookClosed();
+			_updateQuestBookSpritePos();
 		}
 		
 		//update the books sprite positioning to be top left of the view at all times unless opened
@@ -102,11 +164,18 @@ public:
 		std::shared_ptr<Entity> close = EntityManager::GetInstance()->GetEntities("QuestBook-Close")[0];
 		auto bookSc = book->GetComponent<CSprite>();
 		auto closeSc = close->GetComponent<CSprite>();
+		auto questVector = EntityManager::GetInstance()->GetEntities("Quests");
+
+
 		
 		if (_opened) {
 			//render book open relative to view
 
 			ctx->draw(closeSc->sprite);
+			for (auto& e : questVector) {
+				auto txt = e->GetComponent<CText>();
+				ctx->draw(txt->text);
+			}
 		}
 		else {
 			//render book sprite relative to view
